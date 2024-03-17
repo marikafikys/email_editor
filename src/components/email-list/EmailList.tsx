5d456756e34bb3b5e9ca5a0a0df7f4cd0339346d
marikafikys <marikaafikys@gmail.com>
2024-03-17 19:36:41 +0300

    Add layout of the EmailList

--- src/components/email-list/EmailList.tsx
+++ src/components/email-list/EmailList.tsx
@@ -1,7 +1,11 @@
+import styles from "./EmailList.module.scss";
+
 export function EmailList() {
 	return (
-		<div>
-			EmailList
+		<div className={styles.list}>
+			<div>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium recusandae ad asperiores iure fugiat optio blanditiis, hic commodi? Veritatis, quos.
+			</div>
 		</div>
 	);
 }
