aa783a6250ee7ead06e71b7500ae797f78509b4e
marikafikys <marikaafikys@gmail.com>
2024-03-17 19:55:02 +0300

    Add getSelectionText function

--- src/components/email-editor/EmailEditor.tsx
+++ src/components/email-editor/EmailEditor.tsx
@@ -1,16 +1,38 @@
 import { Eraser, Bold, Italic, Underline } from "lucide-react";
 import styles from "./EmailEditor.module.scss";
+import { useRef, useState } from "react";
 
 export function EmailEditor() {
+  const [text, setText] =
+    useState(`Hey! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+  Voluptate debitis provident tempore rerum animi odio distinctio
+  laboriosam ab natus voluptatum.`);
+
+  const textRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const getSelectionText = () => {
+    if (!textRef.current) return;
+    const cursorStart = textRef.current?.selectionStart;
+    const cursorEnd = textRef.current?.selectionEnd;
+    const selectedText = text.substring(cursorStart, cursorEnd);
+
+    if (!selectedText) return;
+  };
+
   return (
     <div>
       <h1>Email Editor</h1>
       <div className={styles.card}>
-        <div className={styles.editor} spellCheck='false'>
-          Hey! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Voluptate debitis provident tempore rerum animi odio distinctio
-          laboriosam ab natus voluptatum.
-        </div>
+        <textarea
+          ref={textRef}
+          className={styles.editor}
+          spellCheck="false"
+          value={text}
+          onChange={e => setText(e.target.value)}
+          onClick={getSelectionText}
+        >
+          {text}
+        </textarea>
         <div className={styles.actions}>
           <div className={styles.tools}>
             <button>
